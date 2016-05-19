afb401a0543e627b00a70fde0aab37e7e8d54853
Pablo Matías Gomez <pablomatiasgomez@gmail.com>
2016-05-19 11:39:29 -0300

    added rava as a source

--- stock.sh
+++ stock.sh
@@ -4,25 +4,41 @@ GREEN='\033[1;32m'
 RED='\033[1;31m'
 NC='\033[0m' # No Color
 
-print_variation() {
-
+get_variation_from_io() {
 	#curl -X GET "https://www.invertironline.com/titulo/cotizacion/BCBA/$1" -s | grep "data-field=\"Variacion\"" | sed s/\(\<span\ data-field=\"Variacion\"\>//g | sed s!\</span\>\%\)!!g | xargs;
 
 	URL="https://www.invertironline.com/titulo/cotizacion/BCBA/$1";
 
 	VARIATION="$(curl -X GET $URL -s | grep data-field=\"Variacion\" | sed s!\</span\>%\)!!g | sed s!\(\<span\>!!g | sed s!\(\<span\ data-field=\"Variacion\"\>!!g | xargs)";
 
-	SIGN=${VARIATION:0:1}
+	echo $VARIATION;
+}
+
+get_variation_from_rava() {
+	#curl -X GET 'http://www.ravaonline.com/v2/empresas/perfil.php?e=erar' -s | grep "font-size:16px;font-weight:bold;color" | sed s/\<td\ style=\"font-family\:\ LatoWeb,\ Arial\"\>//g | sed s/\<span.*d\"\>// | sed s/\<.*//
+
+	URL="http://www.ravaonline.com/v2/empresas/perfil.php?e=$1";
+
+	VARIATION="$(curl -X GET $URL -s | grep "font-size:16px;font-weight:bold;color" | sed s/\<td\ style=\"font-family\:\ LatoWeb,\ Arial\"\>//g | sed s/\<span.*[dkn]\"\>// | sed s/%.*// | xargs)";
+
+	echo $VARIATION;
+}
+
+print_variation() {
+	NAME=$1
+	VARIATION=$2;
+
+	SIGN=${VARIATION:0:1};
 
 	if [ "$SIGN" == "-" ]; then
-		printf "$1 --> ${RED}${VARIATION}%%${NC}\n";
+		printf "$NAME --> ${RED}${VARIATION}%%${NC}\n";
 	else
-		printf "$1 --> ${GREEN}${VARIATION}%%${NC}\n";
+		printf "$NAME --> ${GREEN}${VARIATION}%%${NC}\n";
 	fi;
-
 }
 
 for var in "$@"
 do
-    print_variation "$var"
+	VARIATION="$(get_variation_from_rava $var)";
+	print_variation "$var" "$VARIATION";
 done
