5bd1406cd4bac85d0bbdf8059fc3574263d5e58f
Pablo Matías Gomez <pablomatiasgomez@gmail.com>
2016-07-25 11:52:19 -0300

    added update date

--- stock.js
+++ stock.js
@@ -1,15 +1,21 @@
 #!/usr/bin/env node
 
+Array.prototype.max = function() {
+  return Math.max.apply(null, this);
+};
+
+
 var request = require('request');
 
 var GREEN = '\033[1;32m';
 var RED = '\033[1;31m';
 var NC = '\033[0m'; // No Color
 
-var URL ="http://www.ravaonline.com/v2/empresas/perfil.php?e=";
+var URL_PREFIX ="http://www.ravaonline.com/v2/empresas/perfil.php?e=";
 
 function getAndPrint(stockName) {
-	request(URL + stockName, function(error, response, body) {
+	var url = URL_PREFIX + (stockName.toUpperCase() == "MERV" ? "" : stockName); 
+	request(url, function(error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var lines = body.split("\n");
 
@@ -28,8 +34,9 @@ function getAndPrint(stockName) {
 			if (currentValueLine && variationLine) {
 				var currentValue = currentValueLine.split(">")[2].split("<")[0];
 				var variation = variationLine.split(">")[2].split("<")[0];
+				var update = variationLine.split(">")[4].split("<")[0];
 
-				printValues(stockName, currentValue, variation);
+				printValues(stockName, currentValue, variation, update, url);
 			} else {
 				console.error("Value not found for " + stockName);
 			}
@@ -39,9 +46,16 @@ function getAndPrint(stockName) {
 	});
 }
 
-function printValues(stockName, currentValue, variation) {
+function printValues(stockName, currentValue, variation, update, url) {
 	var color = variation.substring(0, 1) == "-" ? RED : GREEN;
-	console.log(stockName + " --> " + currentValue + " (" + color + variation + NC + ")");
+	console.log(stockName + " --> " + currentValue + " (" + color + variation + NC + ") \t @ " + update + "\t" + url);
 }
 
-process.argv.slice(2).forEach(getAndPrint);
+var stockNames = process.argv.slice(2);
+var maxLength = stockNames.map(function(stock) { return stock.length; }).max();
+for (var i = 0; i < stockNames.length; i++) {
+	while (stockNames[i].length < maxLength) {
+		stockNames[i] = stockNames[i] + " ";
+	}
+}
+stockNames.forEach(getAndPrint);
